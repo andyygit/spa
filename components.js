d083bbdd5c065c40594982915c3cd9080723c7a7
andyygit <android.bartos@gmail.com>
2022-07-15 14:19:39 +0300

    Add files via upload

--- components.js
+++ components.js
@@ -1,89 +1,100 @@
 import { pages } from "./pages.js"
 
 class Navbar extends HTMLElement {
-    constructor() {
-        super()
-        this.pages = pages
-    }
-    getMenuItems() {
-        return this.pages.map(item => `<a href="#" data-target="${item.page}" ${this.getAttribute('activelink') == item.menuitems.title ? "class='active'" : ""}>${item.menuitems.title}</a>`)
-    }
-    generateMenu() {
-        return ''.concat(...this.getMenuItems())
-    }
-    clickHandler(event) {
-        this.render(event.target.dataset.target)
-    }
-    connectedCallback() {
-        this.innerHTML = `
-                        <div class="navbar">
-                            <div class="container">
-                                <nav id="navigation">${this.generateMenu()}</nav>
-                            </div>
-                        </div>
-                        `
-        this.addEventListener('click', this.clickHandler)
-    }
-    disconnectedCallback() {
-        this.removeEventListener('click', this.clickHandler)
-        // console.log('click handler on menu removed')
-    }
+  constructor() {
+    super()
+    this.pages = pages
+  }
+  getMenuItems() {
+    return this.pages.map(
+      (item) =>
+        `<a href="#" data-target="${item.page}" ${
+          this.getAttribute("activelink") == item.menuitems.title
+            ? "class='active'"
+            : ""
+        }>${item.menuitems.title}</a>`
+    )
+  }
+  generateMenu() {
+    return "".concat(...this.getMenuItems())
+  }
+  clickHandler(event) {
+    this.render(event.target.dataset.target)
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="navbar">
+      <div class="container">
+        <nav id="navigation">${this.generateMenu()}</nav>
+      </div>
+    </div>
+    `
+    this.addEventListener("click", this.clickHandler)
+  }
+  disconnectedCallback() {
+    this.removeEventListener("click", this.clickHandler)
+    // console.log('click handler on menu removed')
+  }
 }
 
 class Actionbar extends HTMLElement {
-    constructor() {
-        super()
-        this.pages = pages
-        this.activeLink = document.querySelector('#navigation .active').textContent
+  constructor() {
+    super()
+    this.pages = pages
+    this.activeLink = document.querySelector("#navigation .active").textContent
+  }
+  getActionItems() {
+    let page = this.pages.filter(
+      (item) => item.menuitems.title == this.activeLink
+    )
+    if (page[0].menuitems.actions) {
+      return page[0].menuitems.actions.map(
+        (item) => `<a href="#" data-target="${item[1]}">${item[0]}</a>`
+      )
     }
-    getActionItems() {
-        let page = this.pages.filter(item => item.menuitems.title == this.activeLink)
-        if (page[0].menuitems.actions) {
-            return page[0].menuitems.actions.map(item => `<a href="#" data-target="${item[1]}">${item[0]}</a>`)
-        }
-        return "<p></p>"
-    }
-    generateActions() {
-        return ''.concat(...this.getActionItems())
-    }
-    clickHandler(event) {
-        this.render(event.target.dataset.target)
-    }
-    connectedCallback() {
-        this.innerHTML = `
-                        <div class="actionbar">
-                            <div class="container">
-                                <nav id="actions">${this.generateActions()}</nav>
-                            </div>
-                        </div>
-                        `
-    this.addEventListener('click', this.clickHandler)
-    }
-    disconnectedCallback() {
-        this.removeEventListener('click', this.clickHandler)
-        // console.log('click handler on action removed')
-    }
+    return "<p></p>"
+  }
+  generateActions() {
+    return "".concat(...this.getActionItems())
+  }
+  clickHandler(event) {
+    this.render(event.target.dataset.target)
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="actionbar">
+      <div class="container">
+        <nav id="actions">${this.generateActions()}</nav>
+      </div>
+    </div>
+    `
+    this.addEventListener("click", this.clickHandler)
+  }
+  disconnectedCallback() {
+    this.removeEventListener("click", this.clickHandler)
+    // console.log('click handler on action removed')
+  }
 }
 
 class Card extends HTMLElement {
-    constructor() {
-        super()
-    }
-    connectedCallback() {
-        this.innerHTML = `
-                            <div class="card">
-                                <div class="card-title">
-                                card title card title card title
-                                </div>
-                                <div class="card-body">
-                                card body
-                                </div>
-                            </div>
-                        `
-    }
-    disconnectedCallback() {
-        console.log('card removed')
-    }
+  constructor() {
+    super()
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="card">
+      <div class="card-title">
+        card title card title card title
+      </div>
+      <div class="card-body">
+        card body
+      </div>
+    </div>
+    `
+  }
+  disconnectedCallback() {
+    console.log("card removed")
+  }
 }
 
 export { Navbar, Actionbar, Card }
--- pages.js
+++ pages.js
@@ -1,120 +1,148 @@
 const pages = [
-    {
-        page: 'dashboard',
-        layout: '2col',
-        menuitems: {
-            title: 'Dashboard',
-        },
-        content: {
-            left: {
-                body: async () => {
-                    try {
-                        let response = await fetch('assets/static/scanner.json')
-                        let content = await response.json()
-                        let output = Array.from(content).map(item => `<div class="row d-flex"><div>${item.id}</div><div>${item.sn}</div><div>${item.assnr}</div><div>${item.tip}</div><div>${item.dach}</div><div>${item.dpr}</div><div>${item.depozit}</div><div>${item.subcontractor}</div><div>${item.tura}</div><div>${item.service}</div></div>`)
-                        return ''.concat(...output)
-                    } catch (e) {
-                        console.log("Eroare!")
-                        console.log(e)
-                    }
-                }
-            },
-            right: {
-                body: () => 'Right page dashboard'
-            }
-        }
+  {
+    page: "dashboard",
+    layout: "2col",
+    menuitems: {
+      title: "Dashboard",
     },
-    {
-        page: 'infrastructura',
-        layout: '1col',
-        menuitems: {
-            title: 'Infrastructură',
-            actions: [['Depozite', 'infraDepo'], ['Subcontractori', 'infraSubc'], ['Utilizatori', 'infraUtiliz'], ['Știri', 'infraStiri'], ['Requests for change', 'infraReq']]
+    content: {
+      left: {
+        body: async () => {
+          try {
+            let response = await fetch("assets/static/scanner.json");
+            let content = await response.json();
+            let output = Array.from(content).map(
+              (item) =>
+                `<div class="row d-flex"><div>${item.id}</div><div>${item.sn}</div><div>${item.assnr}</div><div>${item.tip}</div><div>${item.dach}</div><div>${item.dpr}</div><div>${item.depozit}</div><div>${item.subcontractor}</div><div>${item.tura}</div><div>${item.service}</div></div>`
+            );
+            return "".concat(...output);
+          } catch (e) {
+            console.log("Eroare!");
+            console.log(e);
+          }
         },
-        content: {
-            body: '1 column page infrastructura'
-        }
+      },
+      right: {
+        body: () => "Right page dashboard",
+      },
     },
-    {
-        page: 'inventar',
-        layout: '2col',
-        menuitems: {
-            title: 'Inventar',
-            actions: [['Scannere', 'invScannere'], ['Imprimante mobile', 'invPrintere'], ['Echipamente birou', 'invEchipamente'], ['Service', 'invService']]
-        },
-        content: {
-            left: {
-                body: () => 'Left page inventar'
-            },
-            right: {
-                body: () => 'Right page inventar'
-            }
-        }
+  },
+  {
+    page: "infrastructura",
+    layout: "1col",
+    menuitems: {
+      title: "Infrastructură",
+      actions: [
+        ["Depozite", "infraDepo"],
+        ["Subcontractori", "infraSubc"],
+        ["Utilizatori", "infraUtiliz"],
+        ["Știri", "infraStiri"],
+        ["Requests for change", "infraReq"],
+      ],
     },
-    {
-        page: 'solicitari',
-        layout: '1col',
-        menuitems: {
-            title: 'Solicitări',
-            actions: [['Echipamente', 'solicEchip'], ['Consumabile', 'solicCons']]
-        },
-        content: {
-            body: '1 column page solicitari'
-        }
+    content: {
+      body: "1 column page infrastructura",
     },
-    {
-        page: 'cautare',
-        layout: '1col',
-        menuitems: {
-            title: 'Căutare',
-            actions: [['Scanner', 'cautScanner'], ['Imprimantă mobilă', 'cautPrinter']]
-        },
-        content: {
-            body: '1 column page cautare'
-        }
+  },
+  {
+    page: "inventar",
+    layout: "2col",
+    menuitems: {
+      title: "Inventar",
+      actions: [
+        ["Scannere", "invScannere"],
+        ["Imprimante mobile", "invPrintere"],
+        ["Echipamente birou", "invEchipamente"],
+        ["Service", "invService"],
+      ],
     },
-    {
-        page: 'penalizari',
-        layout: '1col',
-        menuitems: {
-            title: 'Penalizări',
-            actions: [['Adăugare', 'penAdaug'], ['Căutare', 'penCaut'], ['Export', 'penExport']]
-        },
-        content: {
-            body: '1 column page penalizari'
-        }
+    content: {
+      left: {
+        body: () => "Left page inventar",
+      },
+      right: {
+        body: () => "Right page inventar",
+      },
     },
-    {
-        page: 'rapoarte',
-        layout: '1col',
-        menuitems: {
-            title: 'Rapoarte',
-            actions: [['Raport de dimineață', 'rapDimineata'], ['Raport de seară', 'rapSeara'], ['Verificări rapoarte', 'rapVerif']]
-        },
-        content: {
-            body: '1 column page rapoarte'
-        }
+  },
+  {
+    page: "solicitari",
+    layout: "1col",
+    menuitems: {
+      title: "Solicitări",
+      actions: [
+        ["Echipamente", "solicEchip"],
+        ["Consumabile", "solicCons"],
+      ],
     },
-    {
-        page: 'suport',
-        layout: '1col',
-        menuitems: {
-            title: 'Suport',
-        },
-        content: {
-            body: '1 column page suport'
-        }
+    content: {
+      body: "1 column page solicitari",
     },
-    {
-        page: 'documentatii',
-        layout: '1col',
-        menuitems: {
-            title: 'Documentații',
-        },
-        content: {
-            body: '1 column page documentatii'
-        }
-    }
-]
+  },
+  {
+    page: "cautare",
+    layout: "1col",
+    menuitems: {
+      title: "Căutare",
+      actions: [
+        ["Scanner", "cautScanner"],
+        ["Imprimantă mobilă", "cautPrinter"],
+      ],
+    },
+    content: {
+      body: "1 column page cautare",
+    },
+  },
+  {
+    page: "penalizari",
+    layout: "1col",
+    menuitems: {
+      title: "Penalizări",
+      actions: [
+        ["Adăugare", "penAdaug"],
+        ["Căutare", "penCaut"],
+        ["Export", "penExport"],
+      ],
+    },
+    content: {
+      body: "1 column page penalizari",
+    },
+  },
+  {
+    page: "rapoarte",
+    layout: "1col",
+    menuitems: {
+      title: "Rapoarte",
+      actions: [
+        ["Raport de dimineață", "rapDimineata"],
+        ["Raport de seară", "rapSeara"],
+        ["Verificări rapoarte", "rapVerif"],
+      ],
+    },
+    content: {
+      body: "1 column page rapoarte",
+    },
+  },
+  {
+    page: "suport",
+    layout: "1col",
+    menuitems: {
+      title: "Suport",
+    },
+    content: {
+      body: "1 column page suport",
+    },
+  },
+  {
+    page: "documentatii",
+    layout: "1col",
+    menuitems: {
+      title: "Documentații",
+    },
+    content: {
+      body: "1 column page documentatii",
+    },
+  },
+];
 
-export { pages }
+export { pages };
